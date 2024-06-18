b0f9181ae73e83844e9af2fad385be9f71b11121
govind kumawat2002 <kumawatritik54@gmail.com>
2024-06-18 15:21:47 +0530

    python project

---
+++ lakhan/contact book/app.py
@@ -0,0 +1,31 @@
+print("Hello baby 😍 \nI hope you are good.👌")
+shope_name = input("enter your shope name :")
+phone_number = input("enter your phone number:")
+email= input("Add your email :")
+address = input("please add your address 😍 :")
+
+bookdictonary = {}
+
+def shope_names(bookdictonary,key ,value):
+    bookdictonary[key] = value
+
+for shope_name in bookdictonary:
+    if 
+
+
+
+
+
+
+
+
+
+# def store_name(dictionary, key, value):
+#     dictionary[key] = value
+
+# # Example usage
+# my_dict = {}
+# store_name(my_dict, 'shope_name',shope_name)
+# store_name(my_dict, "phone_number",phone_number)
+
+# print(my_dict)
